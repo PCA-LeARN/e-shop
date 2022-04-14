2c5f84815740bc24937937a9f88bdeaa4502fab8
Romaric MADEGNAN <ricomadegnan@gmail.com>
2022-04-14 18:16:49 +0100

    Mise a jour du fichier Colors

--- assets/Colors.js
+++ assets/Colors.js
@@ -1,7 +1,13 @@
 export const COLORS = {
-    white: '#fff',
-    black: '#000',
-    blue: '#484496',
-    darkblue:'#34316E'
-    // your colors
-  }
+  white: '#fff',
+  black: '#000',
+  blue: '#484496',
+  darkblue:'#34316E',
+  purple:"#D4D3F2",
+  green:"#59B368",
+  skinblue:"#B9B8E3",
+  gray:"#707070",
+  lsblue:"#CCCCE3",
+  danger:"#A03035"
+  // your colors
+}
